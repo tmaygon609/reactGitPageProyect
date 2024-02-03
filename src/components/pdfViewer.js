e70ecc201ebc9a1a4eea7da7121b6f6658890409
tmaygon609@iesalmudeyne.es <tmaygon609@iesalmudeyne.es>
2024-02-03 20:09:51 +0100

    Puesto de nuevo codigo J.Antonio

--- src/components/pdfViewer.js
+++ src/components/pdfViewer.js
@@ -3,15 +3,13 @@ import React from 'react';
 function PDFViewer({ src }) {
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
-      <object
-        data={src}
-        type="application/pdf"
+      <iframe
+        title="PDF Viewer"
+        src={src}
         width="100%"
         height="100%"
         style={{ border: 'none' }}
-      >
-        <p>El visor de PDF no está disponible. Puedes descargar el archivo <a href={src}>aquí</a>.</p>
-      </object>
+      ></iframe>
     </div>
   );
 }
