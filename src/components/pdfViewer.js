2cfa56e7370e1c02ec19810107b072dc0d140531
tmaygon609@iesalmudeyne.es <tmaygon609@iesalmudeyne.es>
2024-02-03 20:07:29 +0100

    Prueba de objeto pdf

--- src/components/pdfViewer.js
+++ src/components/pdfViewer.js
@@ -3,13 +3,15 @@ import React from 'react';
 function PDFViewer({ src }) {
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
-      <iframe
-        title="PDF Viewer"
-        src={src}
+      <object
+        data={src}
+        type="application/pdf"
         width="100%"
         height="100%"
         style={{ border: 'none' }}
-      ></iframe>
+      >
+        <p>El visor de PDF no está disponible. Puedes descargar el archivo <a href={src}>aquí</a>.</p>
+      </object>
     </div>
   );
 }
